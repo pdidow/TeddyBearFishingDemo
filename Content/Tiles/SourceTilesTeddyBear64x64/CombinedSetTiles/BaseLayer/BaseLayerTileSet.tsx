53a78ee199cca66f38cc322f2180efe471727c3a
pdidow <petermartind@outlook.com>
2025-10-27 11:06:35 -0600

    Full upload of TeddyBearFishingDemo with Git LFS

---
+++ Content/Tiles/SourceTilesTeddyBear64x64/CombinedSetTiles/BaseLayer/BaseLayerTileSet.tsx
@@ -0,0 +1,144 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="BaseLayerTileSet" tilewidth="64" tileheight="64" tilecount="72" columns="9">
+ <image source="BaseLayerTileSet.png" width="576" height="512"/>
+ <tile id="2">
+  <animation>
+   <frame tileid="2" duration="200"/>
+   <frame tileid="3" duration="200"/>
+   <frame tileid="4" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="5">
+  <animation>
+   <frame tileid="5" duration="200"/>
+   <frame tileid="6" duration="200"/>
+   <frame tileid="7" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <animation>
+   <frame tileid="8" duration="200"/>
+   <frame tileid="17" duration="200"/>
+   <frame tileid="70" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="9">
+  <animation>
+   <frame tileid="9" duration="200"/>
+   <frame tileid="10" duration="200"/>
+   <frame tileid="11" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="12">
+  <animation>
+   <frame tileid="12" duration="200"/>
+   <frame tileid="13" duration="200"/>
+   <frame tileid="14" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="19">
+  <animation>
+   <frame tileid="19" duration="200"/>
+   <frame tileid="20" duration="200"/>
+   <frame tileid="21" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="22">
+  <animation>
+   <frame tileid="22" duration="200"/>
+   <frame tileid="23" duration="200"/>
+   <frame tileid="24" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="25">
+  <animation>
+   <frame tileid="25" duration="200"/>
+   <frame tileid="27" duration="200"/>
+   <frame tileid="28" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="29">
+  <animation>
+   <frame tileid="29" duration="200"/>
+   <frame tileid="30" duration="200"/>
+   <frame tileid="31" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="33">
+  <animation>
+   <frame tileid="33" duration="200"/>
+   <frame tileid="34" duration="200"/>
+   <frame tileid="36" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="37">
+  <animation>
+   <frame tileid="37" duration="200"/>
+   <frame tileid="38" duration="200"/>
+   <frame tileid="39" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="40">
+  <animation>
+   <frame tileid="40" duration="200"/>
+   <frame tileid="41" duration="200"/>
+   <frame tileid="42" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="43">
+  <animation>
+   <frame tileid="43" duration="200"/>
+   <frame tileid="45" duration="200"/>
+   <frame tileid="46" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="47">
+  <animation>
+   <frame tileid="47" duration="200"/>
+   <frame tileid="48" duration="200"/>
+   <frame tileid="49" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="50">
+  <animation>
+   <frame tileid="50" duration="200"/>
+   <frame tileid="51" duration="200"/>
+   <frame tileid="52" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="54">
+  <animation>
+   <frame tileid="54" duration="200"/>
+   <frame tileid="55" duration="200"/>
+   <frame tileid="56" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="57">
+  <animation>
+   <frame tileid="57" duration="200"/>
+   <frame tileid="58" duration="200"/>
+   <frame tileid="59" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="60">
+  <animation>
+   <frame tileid="60" duration="200"/>
+   <frame tileid="61" duration="200"/>
+   <frame tileid="63" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="64">
+  <animation>
+   <frame tileid="64" duration="200"/>
+   <frame tileid="65" duration="200"/>
+   <frame tileid="66" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="67">
+  <animation>
+   <frame tileid="67" duration="200"/>
+   <frame tileid="68" duration="200"/>
+   <frame tileid="69" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ Content/Tiles/SourceTilesTeddyBear64x64/TeddyBearFishingTileSet.tsx
@@ -0,0 +1,395 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="TeddyBearFishingTileSet" tilewidth="128" tileheight="64" tilecount="90" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="AnimatedTiles/Water_ShallowDepth1.png"/>
+  <animation>
+   <frame tileid="0" duration="500"/>
+   <frame tileid="1" duration="500"/>
+   <frame tileid="2" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="AnimatedTiles/Water_ShallowDepth2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="AnimatedTiles/Water_ShallowDepth3.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="64" source="StaticTiles/Sand1.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="64" source="AnimatedTiles/Water_ShallowDepthLilly1.png"/>
+  <animation>
+   <frame tileid="8" duration="300"/>
+   <frame tileid="9" duration="300"/>
+   <frame tileid="10" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="9">
+  <image width="64" height="64" source="AnimatedTiles/Water_ShallowDepthLilly2.png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="64" source="AnimatedTiles/Water_ShallowDepthLilly3.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="64" source="AnimatedTiles/FishSpot0.png"/>
+  <animation>
+   <frame tileid="11" duration="300"/>
+   <frame tileid="12" duration="200"/>
+   <frame tileid="13" duration="200"/>
+   <frame tileid="14" duration="200"/>
+   <frame tileid="15" duration="200"/>
+   <frame tileid="16" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="12">
+  <image width="64" height="64" source="AnimatedTiles/FishSpot1.png"/>
+ </tile>
+ <tile id="13">
+  <image width="64" height="64" source="AnimatedTiles/FishSpot2.png"/>
+ </tile>
+ <tile id="14">
+  <image width="64" height="64" source="AnimatedTiles/FishSpot3.png"/>
+ </tile>
+ <tile id="15">
+  <image width="64" height="64" source="AnimatedTiles/FishSpot4.png"/>
+ </tile>
+ <tile id="16">
+  <image width="64" height="64" source="AnimatedTiles/FishSpot5.png"/>
+ </tile>
+ <tile id="17">
+  <image width="64" height="64" source="AnimatedTiles/Water_Deep1.png"/>
+  <animation>
+   <frame tileid="17" duration="300"/>
+   <frame tileid="18" duration="300"/>
+   <frame tileid="19" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="18">
+  <image width="64" height="64" source="AnimatedTiles/Water_Deep2.png"/>
+ </tile>
+ <tile id="19">
+  <image width="64" height="64" source="AnimatedTiles/Water_Deep3.png"/>
+ </tile>
+ <tile id="20">
+  <image width="64" height="64" source="AnimatedTiles/Water_MidDepth1.png"/>
+  <animation>
+   <frame tileid="20" duration="300"/>
+   <frame tileid="21" duration="300"/>
+   <frame tileid="22" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="21">
+  <image width="64" height="64" source="AnimatedTiles/Water_MidDepth2.png"/>
+ </tile>
+ <tile id="22">
+  <image width="64" height="64" source="AnimatedTiles/Water_MidDepth3.png"/>
+ </tile>
+ <tile id="23">
+  <image width="64" height="64" source="AnimatedTiles/CatTail0.png"/>
+  <animation>
+   <frame tileid="23" duration="300"/>
+   <frame tileid="24" duration="300"/>
+   <frame tileid="25" duration="300"/>
+   <frame tileid="26" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="24">
+  <image width="64" height="64" source="AnimatedTiles/CatTail1.png"/>
+ </tile>
+ <tile id="25">
+  <image width="64" height="64" source="AnimatedTiles/CatTail2.png"/>
+ </tile>
+ <tile id="26">
+  <image width="64" height="64" source="AnimatedTiles/CatTail3.png"/>
+ </tile>
+ <tile id="29">
+  <image width="64" height="64" source="StaticTiles/DeckHoriz.png"/>
+ </tile>
+ <tile id="30">
+  <image width="64" height="64" source="StaticTiles/DeckVert.png"/>
+ </tile>
+ <tile id="31">
+  <image width="64" height="64" source="StaticTiles/Grass1.png"/>
+ </tile>
+ <tile id="32">
+  <image width="64" height="64" source="StaticTiles/tree.png"/>
+ </tile>
+ <tile id="33">
+  <image width="128" height="64" source="StaticTiles/Garage.png"/>
+ </tile>
+ <tile id="34">
+  <image width="64" height="64" source="StaticTiles/SandPalm.png"/>
+ </tile>
+ <tile id="35">
+  <image width="64" height="64" source="StaticTiles/SandSkull.png"/>
+ </tile>
+ <tile id="36">
+  <image width="64" height="64" source="StaticTiles/TreeStand.png"/>
+ </tile>
+ <tile id="45">
+  <image width="64" height="64" source="StaticTiles/Sand_GrassBottomLeft.png"/>
+ </tile>
+ <tile id="46">
+  <image width="64" height="64" source="StaticTiles/Sand_GrassBottomRight.png"/>
+ </tile>
+ <tile id="47">
+  <image width="64" height="64" source="StaticTiles/Sand_GrassTopLeft.png"/>
+ </tile>
+ <tile id="48">
+  <image width="64" height="64" source="StaticTiles/Sand_GrassTopRight.png"/>
+ </tile>
+ <tile id="49">
+  <image width="64" height="64" source="StaticTiles/DuckWeed.png"/>
+ </tile>
+ <tile id="50">
+  <image width="64" height="64" source="AnimatedTiles/Grass_WaterBottomRight1.png"/>
+  <animation>
+   <frame tileid="50" duration="200"/>
+   <frame tileid="51" duration="200"/>
+   <frame tileid="52" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="51">
+  <image width="64" height="64" source="AnimatedTiles/Grass_WaterBottomRight2.png"/>
+ </tile>
+ <tile id="52">
+  <image width="64" height="64" source="AnimatedTiles/Grass_WaterBottomRight3.png"/>
+ </tile>
+ <tile id="53">
+  <image width="64" height="64" source="AnimatedTiles/Grass_WaterBottomLeft1.png"/>
+  <animation>
+   <frame tileid="53" duration="200"/>
+   <frame tileid="54" duration="200"/>
+   <frame tileid="55" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="54">
+  <image width="64" height="64" source="AnimatedTiles/Grass_WaterBottomLeft2.png"/>
+ </tile>
+ <tile id="55">
+  <image width="64" height="64" source="AnimatedTiles/Grass_WaterBottomLeft3.png"/>
+ </tile>
+ <tile id="56">
+  <image width="64" height="64" source="AnimatedTiles/Grass_WaterTopRight1.png"/>
+  <animation>
+   <frame tileid="56" duration="200"/>
+   <frame tileid="57" duration="200"/>
+   <frame tileid="58" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="57">
+  <image width="64" height="64" source="AnimatedTiles/Grass_WaterTopRight2.png"/>
+ </tile>
+ <tile id="58">
+  <image width="64" height="64" source="AnimatedTiles/Grass_WaterTopRight3.png"/>
+ </tile>
+ <tile id="59">
+  <image width="64" height="64" source="AnimatedTiles/Grass_WaterTopLeft1.png"/>
+  <animation>
+   <frame tileid="59" duration="200"/>
+   <frame tileid="60" duration="200"/>
+   <frame tileid="61" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="60">
+  <image width="64" height="64" source="AnimatedTiles/Grass_WaterTopLeft2.png"/>
+ </tile>
+ <tile id="61">
+  <image width="64" height="64" source="AnimatedTiles/Grass_WaterTopLeft3.png"/>
+ </tile>
+ <tile id="62">
+  <image width="64" height="64" source="AnimatedTiles/Sand_WaterBottomLeft1.png"/>
+  <animation>
+   <frame tileid="62" duration="200"/>
+   <frame tileid="63" duration="200"/>
+   <frame tileid="64" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="63">
+  <image width="64" height="64" source="AnimatedTiles/Sand_WaterBottomLeft2.png"/>
+ </tile>
+ <tile id="64">
+  <image width="64" height="64" source="AnimatedTiles/Sand_WaterBottomLeft3.png"/>
+ </tile>
+ <tile id="65">
+  <image width="64" height="64" source="AnimatedTiles/Sand_WaterBottomRight1.png"/>
+ </tile>
+ <tile id="66">
+  <image width="64" height="64" source="AnimatedTiles/Sand_WaterBottomRight2.png"/>
+  <animation>
+   <frame tileid="65" duration="200"/>
+   <frame tileid="66" duration="200"/>
+   <frame tileid="67" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="67">
+  <image width="64" height="64" source="AnimatedTiles/Sand_WaterBottomRight3.png"/>
+ </tile>
+ <tile id="68">
+  <image width="64" height="64" source="AnimatedTiles/Sand_WaterTopLeft1.png"/>
+  <animation>
+   <frame tileid="68" duration="200"/>
+   <frame tileid="69" duration="200"/>
+   <frame tileid="70" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="69">
+  <image width="64" height="64" source="AnimatedTiles/Sand_WaterTopLeft2.png"/>
+ </tile>
+ <tile id="70">
+  <image width="64" height="64" source="AnimatedTiles/Sand_WaterTopLeft3.png"/>
+ </tile>
+ <tile id="71">
+  <image width="64" height="64" source="AnimatedTiles/Sand_WaterTopRight1.png"/>
+  <animation>
+   <frame tileid="71" duration="200"/>
+   <frame tileid="72" duration="200"/>
+   <frame tileid="73" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="72">
+  <image width="64" height="64" source="AnimatedTiles/Sand_WaterTopRight2.png"/>
+ </tile>
+ <tile id="73">
+  <image width="64" height="64" source="AnimatedTiles/Sand_WaterTopRight3.png"/>
+ </tile>
+ <tile id="74">
+  <image width="61" height="64" source="StaticTiles/RockyShore.png"/>
+ </tile>
+ <tile id="75">
+  <image width="64" height="64" source="AnimatedTiles/LakeWeed0.png"/>
+  <animation>
+   <frame tileid="75" duration="200"/>
+   <frame tileid="76" duration="200"/>
+   <frame tileid="77" duration="200"/>
+   <frame tileid="78" duration="200"/>
+   <frame tileid="79" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="76">
+  <image width="64" height="64" source="AnimatedTiles/LakeWeed1.png"/>
+ </tile>
+ <tile id="77">
+  <image width="64" height="64" source="AnimatedTiles/LakeWeed2.png"/>
+ </tile>
+ <tile id="78">
+  <image width="64" height="64" source="AnimatedTiles/LakeWeed3.png"/>
+ </tile>
+ <tile id="79">
+  <image width="64" height="64" source="AnimatedTiles/LakeWeed4.png"/>
+ </tile>
+ <tile id="80">
+  <image width="64" height="64" source="AnimatedTiles/Water_ShallowDepth_MidDepth_TopLeft1.png"/>
+  <animation>
+   <frame tileid="80" duration="200"/>
+   <frame tileid="81" duration="200"/>
+   <frame tileid="82" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="81">
+  <image width="64" height="64" source="AnimatedTiles/Water_ShallowDepth_MidDepth_TopLeft2.png"/>
+ </tile>
+ <tile id="82">
+  <image width="64" height="64" source="AnimatedTiles/Water_ShallowDepth_MidDepth_TopLeft3.png"/>
+ </tile>
+ <tile id="83">
+  <image width="64" height="64" source="AnimatedTiles/Water_ShallowDepth_MidDepth_TopRight1.png"/>
+  <animation>
+   <frame tileid="83" duration="200"/>
+   <frame tileid="84" duration="200"/>
+   <frame tileid="85" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="84">
+  <image width="64" height="64" source="AnimatedTiles/Water_ShallowDepth_MidDepth_TopRight2.png"/>
+ </tile>
+ <tile id="85">
+  <image width="64" height="64" source="AnimatedTiles/Water_ShallowDepth_MidDepth_TopRight3.png"/>
+ </tile>
+ <tile id="86">
+  <image width="64" height="64" source="AnimatedTiles/Water_ShallowDepth_MidDepth_BottomLeft1.png"/>
+  <animation>
+   <frame tileid="86" duration="200"/>
+   <frame tileid="87" duration="200"/>
+   <frame tileid="88" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="87">
+  <image width="64" height="64" source="AnimatedTiles/Water_ShallowDepth_MidDepth_BottomLeft2.png"/>
+ </tile>
+ <tile id="88">
+  <image width="64" height="64" source="AnimatedTiles/Water_ShallowDepth_MidDepth_BottomLeft3.png"/>
+ </tile>
+ <tile id="89">
+  <image width="64" height="64" source="AnimatedTiles/Water_ShallowDepth_MidDepth_BottomRight1.png"/>
+  <animation>
+   <frame tileid="89" duration="200"/>
+   <frame tileid="90" duration="200"/>
+   <frame tileid="91" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="90">
+  <image width="64" height="64" source="AnimatedTiles/Water_ShallowDepth_MidDepth_BottomRight2.png"/>
+ </tile>
+ <tile id="91">
+  <image width="64" height="64" source="AnimatedTiles/Water_ShallowDepth_MidDepth_BottomRight3.png"/>
+ </tile>
+ <tile id="92">
+  <image width="64" height="64" source="AnimatedTiles/Water_MidDepth_DeepDepth_BottomLeft1.png"/>
+  <animation>
+   <frame tileid="92" duration="200"/>
+   <frame tileid="93" duration="200"/>
+   <frame tileid="94" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="93">
+  <image width="64" height="64" source="AnimatedTiles/Water_MidDepth_DeepDepth_BottomLeft2.png"/>
+ </tile>
+ <tile id="94">
+  <image width="64" height="64" source="AnimatedTiles/Water_MidDepth_DeepDepth_BottomLeft3.png"/>
+ </tile>
+ <tile id="95">
+  <image width="64" height="64" source="AnimatedTiles/Water_MidDepth_DeepDepth_BottomRight1.png"/>
+  <animation>
+   <frame tileid="95" duration="200"/>
+   <frame tileid="96" duration="200"/>
+   <frame tileid="97" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="96">
+  <image width="64" height="64" source="AnimatedTiles/Water_MidDepth_DeepDepth_BottomRight2.png"/>
+ </tile>
+ <tile id="97">
+  <image width="64" height="64" source="AnimatedTiles/Water_MidDepth_DeepDepth_BottomRight3.png"/>
+ </tile>
+ <tile id="98">
+  <image width="64" height="64" source="AnimatedTiles/Water_MidDepth_DeepDepth_TopLeft1.png"/>
+  <animation>
+   <frame tileid="98" duration="200"/>
+   <frame tileid="99" duration="200"/>
+   <frame tileid="100" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="99">
+  <image width="64" height="64" source="AnimatedTiles/Water_MidDepth_DeepDepth_TopLeft2.png"/>
+ </tile>
+ <tile id="100">
+  <image width="64" height="64" source="AnimatedTiles/Water_MidDepth_DeepDepth_TopLeft3.png"/>
+ </tile>
+ <tile id="101">
+  <image width="64" height="64" source="AnimatedTiles/Water_MidDepth_DeepDepth_TopRight1.png"/>
+  <animation>
+   <frame tileid="101" duration="200"/>
+   <frame tileid="102" duration="200"/>
+   <frame tileid="103" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="102">
+  <image width="64" height="64" source="AnimatedTiles/Water_MidDepth_DeepDepth_TopRight2.png"/>
+ </tile>
+ <tile id="103">
+  <image width="64" height="64" source="AnimatedTiles/Water_MidDepth_DeepDepth_TopRight3.png"/>
+ </tile>
+</tileset>
