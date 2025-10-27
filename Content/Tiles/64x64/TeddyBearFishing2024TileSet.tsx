c733f160051422dcb14487ad531a9bb1849436fb
Peter Didow <petermartind@outlook.com>
2025-10-27 11:29:10 -0600

    Delete Content directory

--- Content/Tiles/64x64/TeddyBearFishing2024TileSet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="TeddyBearFishingSpriteSheet_320x256_64x64" tilewidth="64" tileheight="64" tilecount="20" columns="5">
- <image source="TeddyBearFishingSpriteSheet_320x256_64x64.png" width="320" height="256"/>
-</tileset>
